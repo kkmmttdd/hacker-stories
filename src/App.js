9c8352d868d68b98f6fb63e05cc812bd383212e8
kkmmttdd <kkmmttdd@gmail.com>
2020-09-16 20:47:17 +0900

    Arrow funcf

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+    let text = 'Hello'
+    let list = [
+        1, 3, 4
+    ];
   return (
     <div className="App">
-      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,9 +22,24 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+        <h1>{ text }</h1>
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text" />
+          { list.map(function(i){ return <div key={i}>{i}</div> }) }
+        <List />
     </div>
   );
 }
+
+const List = () => {
+    let list = [
+        5, 6, 7
+    ];
+    return list.map(i =>
+        (
+            <div className="list" key={i} >{ i }</div>
+        )
+    )
+};
 
 export default App;
