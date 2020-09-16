b578a510522735ad1d202605929b983e5053e3a8
kkmmttdd <kkmmttdd@gmail.com>
2020-09-17 02:47:16 +0900

    use state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
@@ -8,37 +7,39 @@ function App() {
     let list = [
         1, 3, 4
     ];
+
+
   return (
     <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* data */}
         <h1>{ text }</h1>
-        <label htmlFor="search">Search: </label>
-        <input id="search" type="text" />
-          { list.map(function(i){ return <div key={i}>{i}</div> }) }
-        <List />
+        {/*array loop*/}
+        { list.map(i => (<div key={i}>{i}</div>)) }
+        {/*use component */}
+        <List list={list} />
+        {/*state */}
+        <Search/>
     </div>
   );
 }
 
-const List = () => {
-    let list = [
-        5, 6, 7
-    ];
-    return list.map(i =>
-        (
-            <div className="list" key={i} >{ i }</div>
-        )
+const List = props => {
+    return props.list.map( i =>
+        (<div className="list" key={i} >{ i }</div>)
+    )
+};
+
+const Search = () => {
+    const [searchTerm, setSearchTerm] = React.useState("");
+    const SearchChanged = (event) => (
+        setSearchTerm(event.target.value)
+    );
+    return (
+        <div>
+            <label htmlFor="search">Search: </label>
+            <input id="search" type="text" onChange={SearchChanged} />
+            <h1>{ searchTerm }</h1>
+        </div>
     )
 };
 
